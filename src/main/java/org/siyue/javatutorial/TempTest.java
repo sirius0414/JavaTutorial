870fe22139d11dee05218e38455e0b2bf1ec64ff
Siyue Zhang <zhangsiyue0414@gmail.com>
2025-09-04 17:45:16 +0800

    Add leetCode package

--- src/main/java/org/siyue/javatutorial/TempTest.java
+++ src/main/java/org/siyue/javatutorial/TempTest.java
@@ -1,23 +1,14 @@
 package org.siyue.javatutorial;
 
-import java.util.Date;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class TempTest {
     public static void main(String[] args) throws InterruptedException {
-        AbstractQueuedSynchronizer aqs = new AbstractQueuedSynchronizer() {
-            @Override
-            protected boolean tryAcquire(int arg) {
-                return false;
-            }
 
-            @Override
-            protected boolean tryRelease(int arg) {
-                return false;
-            }
-        };
-        System.out.println("AbstractQueuedSynchronizer created: " + aqs);
-        ReentrantLock reentrantLock = new ReentrantLock();
     }
 }
