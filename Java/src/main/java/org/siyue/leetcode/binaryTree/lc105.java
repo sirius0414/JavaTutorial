d5f694a4415fbe4b45c91789a44c234d629c6e45
siyue <zhangsiyue0414@gmail.com>
2026-03-03 21:28:27 +0800

    feat(lc105): implement buildTree method for constructing binary tree from preorder and inorder traversal

--- Java/src/main/java/org/siyue/leetcode/binaryTree/lc105.java
+++ Java/src/main/java/org/siyue/leetcode/binaryTree/lc105.java
@@ -17,9 +17,47 @@ public class lc105 {
     }
 
     static class Solution {
+
         public TreeNode buildTree(int[] preorder, int[] inorder) {
-            // TODO: 实现方法
-            return null;
+            TreeNode root = new TreeNode(preorder[0]);
+            return build(preorder, inorder, root);
+        }
+
+        public TreeNode build(int[] preorder, int[] inorder, TreeNode root) {
+
+            if (preorder.length == 0 || inorder.length == 0) {
+                return null;
+            }
+
+            int root_val = preorder[0];
+            root.val = root_val;
+
+            // 左右子树的长度
+            int find_root = 0;
+            while (inorder[find_root] != root_val) {
+                find_root++;
+            }
+            int len_left = find_root;
+            int len_right = inorder.length - len_left - 1;
+
+            // 找到左右子树的值
+            if (len_left != 0) {
+                root.left = new TreeNode(preorder[1]);
+                // 左子树：新的preorder 和 inorder数组
+                int[] l_preorder = Arrays.copyOfRange(preorder, 1, len_left+1);
+                int[] l_inorder = Arrays.copyOfRange(inorder, 0, len_left);
+                // 对子树继续build root.left is new root
+                build(l_preorder, l_inorder, root.left);
+            }
+            if (len_right != 0) {
+                root.right = new TreeNode(preorder[len_left+1]);
+                // 右子树：新的preorder 和 inorder数组
+                int[] r_preorder = Arrays.copyOfRange(preorder, len_left+1, len_left+len_right+1);
+                int[] r_inorder = Arrays.copyOfRange(inorder, len_left+1, len_left+len_right+1);
+                build(r_preorder, r_inorder, root.right);
+            }
+
+            return root;
         }
     }
 
