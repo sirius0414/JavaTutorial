66f869e279ae9a5cdc1fc74dec32252ef35efe84
Siyue Zhang <zhangsiyue0414@gmail.com>
2026-04-28 21:07:15 +0800

    feat(lc22, lc39): implement generateParenthesis and combinationSum methods with backtracking

--- Java/src/main/java/org/siyue/leetcode/backtracking/lc22.java
+++ Java/src/main/java/org/siyue/leetcode/backtracking/lc22.java
@@ -16,8 +16,38 @@ public class lc22 {
 
     static class Solution {
         public List<String> generateParenthesis(int n) {
-            // TODO: 实现方法
-            return new ArrayList<>();
+            Integer left_parenthese = n; // 左括号剩余数量
+            Integer right_parenthese = n; // 右括号剩余数量
+            List<String> result = new ArrayList<>();
+            StringBuilder path = new StringBuilder();
+
+            dfs(left_parenthese, right_parenthese, path, result);
+            return  result;
+        }
+
+        public void dfs(Integer l,  Integer r, StringBuilder path, List<String> result) {
+            // 终止条件，左右括号的剩余数量都是0
+            if (l == 0 && r == 0) {
+                // add path to result
+                result.add(path.toString());
+                return;
+            }
+
+            // 选择左括号，前提是左括号剩余数量大于0
+            if (l > 0) {
+                path.append('(');
+                dfs(l - 1, r, path, result);
+                // 回退，撤销选择
+                path.deleteCharAt(path.length() - 1);
+            }
+
+            // 选择右括号，前提是右括号剩余数量大于0，并且右括号剩余数量必须大于左括号剩余数量
+            if (r > 0 && r > l) {
+                path.append(')');
+                dfs(l, r - 1, path, result);
+                // 回退，撤销选择
+                path.deleteCharAt(path.length() - 1);
+            }
         }
     }
 }
--- Java/src/main/java/org/siyue/leetcode/backtracking/lc39.java
+++ Java/src/main/java/org/siyue/leetcode/backtracking/lc39.java
@@ -18,8 +18,29 @@ public class lc39 {
 
     static class Solution {
         public List<List<Integer>> combinationSum(int[] candidates, int target) {
-            // TODO: 实现方法
-            return new ArrayList<>();
+            List<List<Integer>> result = new ArrayList<>();
+            List<Integer> path = new ArrayList<>();
+            dfs(candidates, target,0, result, path);
+            return result;
+        }
+
+        void dfs(int[] candidates, int target, int index, List<List<Integer>> result, List<Integer> path) {
+                if (target == 0) {
+                    // target的剩余值为0，说明当前路径的和等于目标值，将当前路径加入结果集
+                    result.add(new ArrayList<>(path));
+                    return;
+                }
+
+                for (int i = index; i < candidates.length; i++) {
+                    if (candidates[i] > target) {
+                        continue; // 当前元素大于目标值，超了，跳过
+                    }
+
+                    path.add(candidates[i]); // 选择当前元素，加入路径
+                    dfs(candidates, target - candidates[i], i, result, path); // 递归处理剩余目标值
+
+                    path.remove(path.size() - 1); // 回退，撤销选择
+                }
         }
     }
 }
