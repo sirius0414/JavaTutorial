9eaf18a09aa5741f8dcffd11703fd76b654bad10
Siyue Zhang <zhangsiyue0414@gmail.com>
2026-05-06 14:17:45 +0800

    feat(lc79): implement exist method using DFS for word search in a 2D board

--- Java/src/main/java/org/siyue/leetcode/backtracking/lc79.java
+++ Java/src/main/java/org/siyue/leetcode/backtracking/lc79.java
@@ -21,8 +21,46 @@ public class lc79 {
 
     static class Solution {
         public boolean exist(char[][] board, String word) {
-            // TODO: 实现方法
+            // 全零矩阵来记录哪个位置被用过了
+            boolean[][] visited = new boolean[board.length][board[0].length];
+
+            // 遍历每个位置，作为起点进行DFS
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (dfs(board, word, 0, i, j, visited)) {
+                        return true; // 找到了一条路径，返回true
+                    }
+                }
+            }
+
+            // 没找到咯
             return false;
+        }
+
+        // DFS
+        boolean dfs(char[][] board, String word, int index, int x, int y, boolean[][] visited) {
+            if (index == word.length()) {
+                return true;
+            }
+
+            if (x < 0 || x >= board.length ||
+                    y < 0 || y >= board[0].length ||
+                    visited[x][y] ||
+                    board[x][y] != word.charAt(index)) {
+                return false;
+            }
+
+            visited[x][y] = true;
+
+            boolean found =
+                    dfs(board, word, index + 1, x - 1, y, visited) ||
+                            dfs(board, word, index + 1, x + 1, y, visited) ||
+                            dfs(board, word, index + 1, x, y - 1, visited) ||
+                            dfs(board, word, index + 1, x, y + 1, visited);
+
+            visited[x][y] = false;
+
+            return found;
         }
     }
 }
